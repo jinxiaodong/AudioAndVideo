a3bbd31f55bbe39a122e61818cc12b28ef8d729f
jinxiaodong <xiaodong.jin@chinaredstar.com>
2018-06-26 11:11:15 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaodong/audioandvideo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xiaodong.audioandvideo;
+
+
+import com.xiaodong.basetools.base.JBaseActivity;
+
+public class MainActivity extends JBaseActivity {
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_main;
+    }
+
+
+}
